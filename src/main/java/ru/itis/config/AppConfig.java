ade17c70ac60fe3cc3b5785d90fd821c358ca71e
Daria.Vdovinova <dasha.vdovinova@gmail.com>
2026-03-13 22:08:11 +0300

    третья домашка

--- src/main/java/ru/itis/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package ru.itis.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("ru.itis")
-public class AppConfig {
-}
